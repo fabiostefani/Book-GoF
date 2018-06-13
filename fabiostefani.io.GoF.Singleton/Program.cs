341c39fed277b8f89f73fd5a1ea8a6099952696c
fabiostefani@gmail.com <fabiostefani@gmail.com>
2018-06-12 21:57:46 -0300

    Singleton

---
+++ fabiostefani.io.GoF.Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fabiostefani.io.GoF.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInstance();
+            GetInstance();
+            GetInstance();
+            GetInstance();
+            GetInstance();
+
+            GetInstance2();
+            GetInstance2();
+            GetInstance2();
+            GetInstance2();
+
+            Console.ReadKey();
+        }
+
+        private static void GetInstance()
+        {
+            var instance = Singleton.GetInstance();
+            Console.WriteLine(instance.GetHashCode());
+        }
+
+        private static void GetInstance2()
+        {
+            var instance = Singleton2.GetInstance();
+            Console.WriteLine("Singleton2 " + instance.GetHashCode());
+        }
+    }
+}
---
+++ fabiostefani.io.GoF.Singleton/Singleton2 .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fabiostefani.io.GoF.Singleton
+{
+    public class Singleton2
+    {
+        private static readonly Singleton2 _instance = new Singleton2();
+        private Singleton2()
+        {
+
+        }
+
+        public static Singleton2 GetInstance()
+        {
+            return _instance;
+        }
+    }
+}
